7eb0c5b21f03d2ae3adb4743c386a48f55340fab
mjsdev7 <m.j.swales@hotmail.com>
2026-08-03 10:06:03 +0900

    Add user login authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,6 +21,36 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    res.json({
+      message: "Login successful",
+      userId: user._id
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 module.exports = {
-  registerUser
+  registerUser,
+  loginUser
 };
