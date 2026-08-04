49b83ff3d3a94eb3b205bb012739721f170ec78e
mjsdev7 <m.j.swales@hotmail.com>
2026-08-04 09:05:29 +0900

    Prevent duplicate favourites and secure deletion

--- server/controllers/favouriteController.js
+++ server/controllers/favouriteController.js
@@ -2,6 +2,17 @@ const Favourite = require('../models/Favourite');
 
 const addFavourite = async (req, res) => {
   try {
+    const existingFavourite = await Favourite.findOne({
+      user: req.user.userId,
+      meal: req.body.meal
+    });
+
+    if (existingFavourite) {
+      return res.status(400).json({
+        message: "Meal is already in favourites"
+      });
+    }
+
     const favourite = await Favourite.create({
       user: req.user.userId,
       meal: req.body.meal
@@ -31,7 +42,18 @@ const getFavourites = async (req, res) => {
 
 const deleteFavourite = async (req, res) => {
   try {
-    await Favourite.findByIdAndDelete(req.params.id);
+    const favourite = await Favourite.findOne({
+      _id: req.params.id,
+      user: req.user.userId
+    });
+
+    if (!favourite) {
+      return res.status(404).json({
+        message: "Favourite not found"
+      });
+    }
+
+    await favourite.deleteOne();
 
     res.json({
       message: "Favourite deleted"
