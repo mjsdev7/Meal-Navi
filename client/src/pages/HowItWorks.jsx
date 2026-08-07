a97e7961126d3801cd25444d875b75b7b9782a02
mjsdev7 <m.j.swales@hotmail.com>
2026-08-07 12:23:07 +0900

    Add React Router pages and navigation

---
+++ client/src/pages/HowItWorks.jsx
@@ -0,0 +1,10 @@
+function HowItWorks() {
+  return (
+    <div>
+      <h1>How It Works</h1>
+      <p>Plan your meals, organise your shopping, and make cooking easier.</p>
+    </div>
+  );
+}
+
+export default HowItWorks;
---
+++ client/src/pages/Planner.jsx
@@ -0,0 +1,9 @@
+function Planner() {
+  return (
+    <div>
+      <h1>Planner</h1>
+    </div>
+  );
+}
+
+export default Planner;
