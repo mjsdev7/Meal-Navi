eb99ba2607244647a03156022b459e11c093ac94
mjsdev7 <m.j.swales@hotmail.com>
2026-08-07 22:14:38 +0900

    Create Material UI login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,42 @@
+import "./Login.css";
+import { TextField, Button } from "@mui/material";
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <section className="login-page">
+      <div className="login-card">
+        <h1>Welcome Back</h1>
+        <p>Log in to continue planning your meals.</p>
+
+        <form className="login-form">
+          <TextField label="Email" type="email" fullWidth margin="normal" />
+
+          <TextField
+            label="Password"
+            type="password"
+            fullWidth
+            margin="normal"
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{
+              marginTop: "20px",
+              padding: "12px",
+              borderRadius: "25px",
+              backgroundColor: "#F7B267",
+              "&:hover": {
+                backgroundColor: "#E89A5A",
+              },
+            }}
+          >
+            Log In
+          </Button>
+        </form>
+      </div>
+    </section>
   );
 }
 
