2a6277d0ac6be39208112ac0cfacc2daccbfe7ef
mjsdev7 <m.j.swales@hotmail.com>
2026-08-30 16:37:49 +0900

    Add MealNavi copyright footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,11 @@ import Hero from "../components/Hero";
 
 function Home() {
   return (
-    <>
+    <div className="home-page">
       <Hero />
-    </>
+
+      <footer className="footer">&copy; MealNavi 2026</footer>
+    </div>
   );
 }
 
